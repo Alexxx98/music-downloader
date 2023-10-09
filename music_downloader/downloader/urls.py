00b6c09f1f949592995cdf0e85c89876e04b704d
Aleksander Pres <aleksander.pres98@gmail.com>
2023-10-09 13:38:35 +0200

    Improve page layout

--- music_downloader/downloader/urls.py
+++ music_downloader/downloader/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name="login"),
-    path("connect", views.connect, name="connect"),
+    path("", views.connect, name="connect"),
+    path("login", views.login, name="login"),
     path('redirect', views.callback, name="callback"),
     path('home', views.home, name="home"),
 ]
